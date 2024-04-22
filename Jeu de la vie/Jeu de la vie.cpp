651cd03ea338688d7e7b27460fdeff73dd6e0bec
Froliano <froliano.yt@gmail.com>
2024-04-22 15:01:56 +0200

    jeux terminer besoin d'ajout la methode pour changer la couleur d'une case avec un click souris

--- Jeu de la vie/Jeu de la vie.cpp
+++ Jeu de la vie/Jeu de la vie.cpp
@@ -1,140 +1,145 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-const int WIDTH = 300;
-const int HEIGHT = 300;
-const int ROWS = 3;
-const int COLS = 3;
+const int SQUARE_SIZE = 100;
+const int WIDTH = 700;
+const int HEIGHT = 400;
 
-enum class Player { None, X, O };
 
-class TicTacToe {
-public:
-    TicTacToe() : currentPlayer(Player::X), window(sf::VideoMode(WIDTH, HEIGHT), "Tic Tac Toe") {
-        board = std::vector<std::vector<Player>>(ROWS, std::vector<Player>(COLS, Player::None));
-    }
+class Game
+{
+    private:
+        std::vector<std::vector<bool>> board;
+        sf::RenderWindow window;
 
-    void run() {
-        while (window.isOpen()) {
-            sf::Event event;
-            while (window.pollEvent(event)) {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-                else if (event.type == sf::Event::MouseButtonPressed)
-                    handleClick(event.mouseButton.x, event.mouseButton.y);
-            }
-
-            window.clear(sf::Color::White); // Efface la fenêtre avec une couleur blanche
-            drawBoard();
-            window.display(); // Affiche le contenu de la fenêtre
-        }
-    }
-
-private:
-    Player currentPlayer;
-    std::vector<std::vector<Player>> board;
-    sf::RenderWindow window;
-
-    void drawBoard() {
-        sf::RectangleShape line(sf::Vector2f(10, HEIGHT));
-        line.setFillColor(sf::Color::Black);
-        line.setPosition(WIDTH / 3 - 5, 0);
-        window.draw(line);
-        line.setPosition(2 * WIDTH / 3 - 5, 0);
-        window.draw(line);
-        line.setSize(sf::Vector2f(WIDTH, 10));
-        line.setRotation(90);
-        line.setPosition(0, HEIGHT / 3 - 5);
-        window.draw(line);
-        line.setPosition(0, 2 * HEIGHT / 3 - 5);
-        window.draw(line);
-
-        for (int i = 0; i < ROWS; ++i) {
-            for (int j = 0; j < COLS; ++j) {
-                if (board[i][j] == Player::X) {
-                    drawX(i, j);
-                }
-                else if (board[i][j] == Player::O) {
-                    drawO(i, j);
+        void drawBoard() {
+            for (int x = 0; x < WIDTH/SQUARE_SIZE; ++x)
+            {
+                for (int y = 0; y < HEIGHT/SQUARE_SIZE; ++y)
+                {
+                    if (board[x][y]) {
+                        fillBlack(x, y);
+                    }
+                    else
+                    {
+                        fillWhite(x, y);
+                    }
                 }
             }
-        }
-    }
 
-    void drawX(int row, int col) {
-        sf::RectangleShape line1(sf::Vector2f(WIDTH / 3 - 20, 5));
-        line1.setFillColor(sf::Color::Black);
-        line1.rotate(45);
-        line1.setPosition(col * WIDTH / 3 + 10, row * HEIGHT / 3 + 10);
-        window.draw(line1);
-
-        sf::RectangleShape line2(sf::Vector2f(WIDTH / 3 - 20, 5));
-        line2.setFillColor(sf::Color::Black);
-        line2.rotate(-45);
-        line2.setPosition(col * WIDTH / 3 + 10, (row + 1) * HEIGHT / 3 - 10);
-        window.draw(line2);
-    }
-
-    void drawO(int row, int col) {
-        sf::CircleShape circle(WIDTH / 6 - 10);
-        circle.setFillColor(sf::Color::Transparent);
-        circle.setOutlineThickness(5);
-        circle.setOutlineColor(sf::Color::Black);
-        circle.setPosition(col * WIDTH / 3 + 10, row * HEIGHT / 3 + 10);
-        window.draw(circle);
-    }
-
-    void handleClick(int x, int y) {
-        int row = y / (HEIGHT / 3);
-        int col = x / (WIDTH / 3);
-
-        if (row >= 0 && row < ROWS && col >= 0 && col < COLS && board[row][col] == Player::None) {
-            board[row][col] = currentPlayer;
-            if (checkWin(currentPlayer)) {
-                std::cout << "Player " << (currentPlayer == Player::X ? 'X' : 'O') << " wins!" << std::endl;
-                window.close();
+            sf::RectangleShape line(sf::Vector2f(1, HEIGHT));
+            line.setFillColor(sf::Color(70, 70, 70));
+            for (int i = 0; i <= WIDTH; i += SQUARE_SIZE)
+            {
+                line.setPosition(i, 0);
+                window.draw(line);
             }
-            else if (checkDraw()) {
-                std::cout << "It's a draw!" << std::endl;
-                window.close();
-            }
-            else {
-                currentPlayer = (currentPlayer == Player::X) ? Player::O : Player::X;
-            }
-        }
-    }
 
-    bool checkWin(Player player) {
-        // Check rows and columns
-        for (int i = 0; i < ROWS; ++i) {
-            if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
-                return true;
-            if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
-                return true;
+            line.setSize(sf::Vector2f(WIDTH, 1));
+            for (int i = 0; i <= HEIGHT; i += SQUARE_SIZE)
+            {
+                line.setPosition(0, i);
+                window.draw(line);
+            }
+
         }
 
-        // Check diagonals
-        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-            return true;
-        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-            return true;
+        void fillWhite(int x, int y)
+        {
+            sf::RectangleShape rect(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
+            rect.setFillColor(sf::Color::White);
+            rect.setPosition(x*SQUARE_SIZE, y*SQUARE_SIZE);
+            window.draw(rect);
+        }
+        
+        void fillBlack(int x, int y)
+        {
+            sf::RectangleShape rect(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
+            rect.setFillColor(sf::Color::Black);
+            rect.setPosition(x * SQUARE_SIZE, y * SQUARE_SIZE);
+            window.draw(rect);
+        }
 
-        return false;
-    }
+        void nextGeneration()
+        {
+            std::vector<std::vector<bool>> newBoard = std::vector<std::vector<bool>>(WIDTH / SQUARE_SIZE, std::vector<bool>(HEIGHT / SQUARE_SIZE, false));
 
-    bool checkDraw() {
-        for (int i = 0; i < ROWS; ++i) {
-            for (int j = 0; j < COLS; ++j) {
-                if (board[i][j] == Player::None)
-                    return false;
+            for (int x = 0; x < WIDTH / SQUARE_SIZE; ++x)
+            {
+                for (int y = 0; y < HEIGHT / SQUARE_SIZE; ++y)
+                {
+                    int nbCellulesAliveNear = 0;
+
+                    for (int i = std::max(0, x - 1); i < std::min(WIDTH / SQUARE_SIZE, x + 2); i++)
+                    {
+                        for (int j = std::max(0, y - 1); j < std::min(HEIGHT / SQUARE_SIZE, y + 2); j++)
+                        {
+                            if (board[i][j] and (i != x or j != y))
+                            {
+                                nbCellulesAliveNear++;
+                            }
+                        }
+                    }
+
+                    if (board[x][y])
+                    {
+                        if (nbCellulesAliveNear == 2 or nbCellulesAliveNear == 3)
+                        {
+                            newBoard[x][y] = true;
+                        }
+                    }
+
+                    else
+                    {
+                        if (nbCellulesAliveNear == 3)
+                        {
+                            newBoard[x][y] = true;
+                        }
+                    }
+                }
+            }
+            board = newBoard;
+        }
+
+    public:
+        Game() : window(sf::VideoMode(WIDTH+1, HEIGHT+1), "Le Jeu de la Vie") {
+            board = std::vector<std::vector<bool>>(WIDTH/SQUARE_SIZE, std::vector<bool>(HEIGHT/SQUARE_SIZE, false));
+            board[2][1] = true;
+            board[2][2] = true;
+            board[2][3] = true;
+        }
+
+        void run()
+        {
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while(window.pollEvent(event)) 
+                {
+                    if(event.type == sf::Event::Closed)
+                        window.close();
+                    else if (event.type == sf::Event::KeyPressed)
+                    {
+                        if (event.key.code == sf::Keyboard::Enter)
+                        {
+                            nextGeneration();
+                        }
+                    }
+                    /*else if (event.type == sf::Event::MouseButtonPressed)
+                        handleClick(event.mouseButton.x, event.mouseButton.y);*/
+                }
+
+                window.clear(sf::Color::White);
+                drawBoard();
+                window.display();
             }
         }
-        return true;
-    }
 };
 
 int main() {
-    TicTacToe game;
+
+    Game game;
+
     game.run();
 
     return 0;
